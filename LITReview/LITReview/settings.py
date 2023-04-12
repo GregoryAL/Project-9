2d9d2e9b47b4362fac6032ad31eeec88325e00a3
Gregory ALLEN <gregory.allen.larut@live.com>
2023-04-12 17:09:22 +0200

    added login

--- LITReview/LITReview/settings.py
+++ LITReview/LITReview/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = "LITReview.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR.joinpath('templates'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,7 +121,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'authentication.User'
