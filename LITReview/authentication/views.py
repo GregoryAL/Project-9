de489a52ab7c3ff228b613d0d34d1af6aa6f49ac
Gregory ALLEN <gregory.allen.larut@live.com>
2023-04-13 10:23:02 +0200

    added signup page

--- LITReview/authentication/views.py
+++ LITReview/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . import forms
 from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
 
 
 def login_page(request):
@@ -15,7 +16,7 @@ def login_page(request):
             )
             if user is not None:
                 login(request, user)
-                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+                return redirect('home')
             else:
                 message = 'Identifiants invalides.'
     return render(
@@ -25,3 +26,14 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
