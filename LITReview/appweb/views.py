15d657fa9d74d171432128ba7342da00c79a034d
Gregory ALLEN <gregory.allen.larut@live.com>
2023-05-11 06:25:15 +0200

    modified home so only create a review button shows modified posts so only modify/delete buttons show

--- LITReview/appweb/views.py
+++ LITReview/appweb/views.py
@@ -1,4 +1,7 @@
 from itertools import chain
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from appweb.forms import TicketCreation, FollowingForm, ReviewCreation, ReviewCreationWithoutTicket
@@ -13,7 +16,6 @@ def home(request):
         Q(user=request.user) |
         Q(user__followed_by__user=request.user)
     ).annotate(content_type=Value('Review', CharField()))
-
     ticketsWithoutReview = Ticket.objects.filter(
         Q(user=request.user) |
         Q(user__followed_by__user=request.user)
@@ -32,26 +34,25 @@ def posts(request):
     reviews = Review.objects.filter(
         Q(user=request.user)
     ).annotate(content_type=Value('Review', CharField()))
-
-    ticketsWithoutReview = Ticket.objects.filter(
+    tickets = Ticket.objects.filter(
         Q(user=request.user)
-    ).distinct().annotate(content_type=Value('TicketWithoutReview', CharField())).exclude(~Q(review=None))
-    ticketsWithReview = Ticket.objects.filter(
-        Q(user=request.user) &
-        ~Q(review__user=request.user)
-    ).distinct().annotate(content_type=Value('TicketWithReview', CharField())).exclude(Q(review=None))
-    ownTicketReview = Ticket.objects.filter(
-        Q(user=request.user) &
+    ).distinct().annotate(content_type=Value('Ticket', CharField()))
+    reviewsTicket = Ticket.objects.filter(
         Q(review__user=request.user)
-    ).distinct().annotate(content_type=Value('OwnTicketReview', CharField())).exclude(Q(review=None))
-    ticketsAndReviews = sorted(chain(ticketsWithoutReview, ticketsWithReview, reviews), key=lambda x: x.time_created, reverse=True)
-    return render(request, 'appweb/posts.html', {'posts': ticketsAndReviews, 'ownticket': ownTicketReview})
+    ).distinct().annotate(content_type=Value('ReviewsTicket', CharField()))
+    ticketsAndReviews = sorted(chain(tickets, reviews), key=lambda x: x.time_created, reverse=True)
+    return render(request, 'appweb/posts.html', {'posts': ticketsAndReviews, 'reviewsticket': reviewsTicket})
 
 
 @login_required
 def subscription(request):
     usersfollowing = UserFollows.objects.filter(user=request.user.id)
     usersfollowed = UserFollows.objects.filter(followed_user=request.user.id)
+    errormessages = {
+        'donotexist': "L'utilisateur n'existe pas.",
+        'autosubscribed': "Vous ne pouvez pas vous abonnez à vous même.",
+        'alreadysubscrided': "Vous êtes déjà abonné(e) à cet utilisateur."
+    }
     if request.method == "GET":
         form = FollowingForm()
         context = {
@@ -64,53 +65,99 @@ def subscription(request):
         form = FollowingForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-            followed_user = get_object_or_404(User, username=username)
-            subscript = UserFollows.objects.create(followed_user=followed_user, user=request.user)
-            subscript.save()
-            return redirect('home')
+            try:
+                followed_user = User.objects.get(username=username)
+            except ObjectDoesNotExist:
+                message = "L'utilisateur n'existe pas."
+            else:
+                if followed_user == request.user:
+                    message = "Vous ne pouvez pas vous abonnez à vous même."
+                else:
+                    try:
+                        subscript = UserFollows.objects.create(followed_user=followed_user, user=request.user)
+                    except IntegrityError:
+                        message = "Vous êtes déjà abonné(e) à cet utilisateur."
+                    else:
+                        subscript.save()
+                        message = "Vous vous êtes bien abonné à " + str(followed_user.username) + "."
+                    finally:
+                        pass
+            finally:
+                context = {
+                    'form': FollowingForm(),
+                    'usersfollowing': usersfollowing,
+                    'usersfollowed': usersfollowed,
+                    'message': message
+                }
+                return render(request, 'appweb/subscription.html', context=context)
 
 
 @login_required
 def deletesubscription(request, id_subscription):
     if request.method == 'GET':
         usersfollowing = get_object_or_404(UserFollows, pk=id_subscription)
-        usersfollowing.delete()
-        return redirect('subscription')
+        if usersfollowing.user == request.user:
+            usersfollowing.delete()
+            return redirect('subscription')
+        else:
+            return redirect('home')
+
+@login_required
+def deletereview(request, id_review):
+    if request.method == 'GET':
+        reviewToDelete = get_object_or_404(Review, pk=id_review)
+        if reviewToDelete.user == request.user:
+            reviewToDelete.delete()
+            return redirect('posts')
+
+@login_required
+def deleteticket(request, id_ticket):
+    if request.method == 'GET':
+        ticketToDelete = get_object_or_404(Ticket, pk=id_ticket)
+        if ticketToDelete.user == request.user:
+            ticketToDelete.delete()
+            return redirect('posts')
 
 
 @login_required
 def ticket_creation(request, id_ticket=None):
     instance_ticket = Ticket.objects.get(pk=id_ticket) if id_ticket is not None else None
-    if request.method == "GET":
-        form = TicketCreation(instance=instance_ticket)
-        return render(request, 'appweb/ticketcreation.html', {'form': form})
-    elif request.method == "POST":
-        form = TicketCreation(request.POST, instance=instance_ticket)
-        if form.is_valid():
-            ticket = form.save(commit=False)
-            ticket.user = request.user
-            ticket.save()
-            return redirect('home')
+    if id_ticket is not None and instance_ticket.user.id != request.user.id:
+        return redirect('home')
+    else:
+        if request.method == "GET":
+            form = TicketCreation(instance=instance_ticket)
+            return render(request, 'appweb/ticketcreation.html', {'form': form})
+        elif request.method == "POST":
+            form = TicketCreation(request.POST, instance=instance_ticket)
+            if form.is_valid():
+                ticket = form.save(commit=False)
+                ticket.user = request.user
+                ticket.save()
+                return redirect('home')
 
 
 @login_required
 def review_modification(request, id_review):
     instance_review = Review.objects.get(pk=id_review)
     instance_ticket = Ticket.objects.get(pk=instance_review.ticket.id)
-    if request.method == "GET":
-        form = ReviewCreation(instance=instance_review)
-        context = {
-            'form': form,
-            'instance_ticket': instance_ticket
-            }
-        return render(request, 'appweb/reviewmodification.html', context=context)
-    elif request.method == "POST":
-        form = ReviewCreation(request.POST, instance=instance_review)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.user
-            review.save()
-            return redirect('home')
+    if instance_review.user.id == request.user.id:
+        if request.method == "GET":
+            form = ReviewCreation(instance=instance_review)
+            context = {
+                'form': form,
+                'instance_ticket': instance_ticket
+                }
+            return render(request, 'appweb/reviewmodification.html', context=context)
+        elif request.method == "POST":
+            form = ReviewCreation(request.POST, instance=instance_review)
+            if form.is_valid():
+                review = form.save(commit=False)
+                review.user = request.user
+                review.save()
+                return redirect('home')
+    else:
+        return redirect('home')
 
 
 @login_required
