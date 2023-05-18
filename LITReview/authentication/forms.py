cd8bcadbc35e953a63c7850564e9bf2b8a412f25
Gregory ALLEN <gregory.allen.larut@live.com>
2023-05-18 08:35:22 +0200

    Modified css and template for login signup to match wireframes

--- LITReview/authentication/forms.py
+++ LITReview/authentication/forms.py
@@ -1,16 +1,48 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    username = forms.CharField(max_length=63, label='Nom d’utilisateur', widget=forms.TextInput(attrs={'placeholder': "Nom d'utilisateur"}))
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput(attrs={'placeholder': "Mot de passe"}), label='Mot de passe')
 
 
-class SignupForm(UserCreationForm):
+class SignupForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'placeholder': "Mot de passe"}),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={'placeholder': "Confirmez votre mot de passe"}),
+    )
+    help_texts = {
+        'password1': None,
+        'password2': None
+    }
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         help_texts = {
-            'password1': None,
             'username': None
         }
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': "Nom d'utilisateur"})
+        }
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
